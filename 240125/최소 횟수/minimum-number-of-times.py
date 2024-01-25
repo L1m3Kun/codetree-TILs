ffb009551e0cd04c7063f3e0e78334c345714742
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2024-01-25 15:34:07 +0900

    Title: 최소 횟수, Time: 309ms, Memory: 29MB, Status: Passed - CodeTree

--- 240125/최소 횟수/minimum-number-of-times.py
+++ 240125/최소 횟수/minimum-number-of-times.py
@@ -6,29 +6,24 @@ def solution():
     a, b = map(int, input().split())
     que = []
     heapq.heappush(que, (0, a))
-    visited= [0]*(b+1)
-    min_v = b+1
+    visited= [0]*100_001
+    visited[a] = 1
+    minv = 100_001
     while que:
         cnt, now = heapq.heappop(que)
         if now == b:
-            min_v = min(min_v, cnt)
-            continue
-        if cnt >= min_v:
-            continue
-        if now+1<=b:
-            heapq.heappush(que,(cnt+1, now+1))
-            visited[now+1] = visited[now] + 1
-        if now-1 >0:
-            heapq.heappush(que,(cnt+1, now-1))
-            visited[now-1] = visited[now] + 1
-        if (now << 1) <= b:
-            heapq.heappush(que,(cnt, now<<1))
-            visited[now<<1] = visited[now]
-            
-    print(min_v)  
+            minv = min(minv, cnt)
+        for next in [now-1, now+1, now*2]:
+            if 0 <= next < 100_000 and not visited[next]:
+                if next == now*2:
+                    visited[next] = 1
+                    heapq.heappush(que, (cnt, next))
+                else:
+                    visited[next] = 1
+                    heapq.heappush(que, (cnt+1, next))
             
 
-
+    print(minv)
 
 if __name__ == "__main__":
     solution()
