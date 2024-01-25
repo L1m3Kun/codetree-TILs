053e68a1e19023c706ef409c6ba366eb58f74edf
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2024-01-25 15:18:10 +0900

    Title: 최소 횟수, Time: 5100ms, Memory: 243MB, Status: Time Limit Exceed - CodeTree

---
+++ 240125/최소 횟수/minimum-number-of-times.py
@@ -0,0 +1,34 @@
+import sys, heapq
+input = sys.stdin.readline
+
+
+def solution():
+    a, b = map(int, input().split())
+    que = []
+    heapq.heappush(que, (0, a))
+    visited= [0]*(b+a+1)
+    min_v = b+1
+    while que:
+        cnt, now = heapq.heappop(que)
+        if now == b:
+            min_v = min(min_v, cnt)
+            continue
+        if cnt >= min_v:
+            continue
+        if now+1<=b+a:
+            heapq.heappush(que,(cnt+1, now+1))
+            visited[now+1] = visited[now] + 1
+        if now-1 >0:
+            heapq.heappush(que,(cnt+1, now-1))
+            visited[now-1] = visited[now] + 1
+        if now << 1 <= b+a:
+            heapq.heappush(que,(cnt, now<<1))
+            visited[now<<1] = visited[now]
+            
+    print(min_v)  
+            
+
+
+
+if __name__ == "__main__":
+    solution()
