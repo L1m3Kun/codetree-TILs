8d89368fd7c214076c1cd5927e48454ba536f0aa
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2024-02-05 21:47:40 +0900

    Title: 단어 삭제, Time: 218ms, Memory: 24MB, Status: Passed - CodeTree

--- 240205/단어 삭제/word-delete.py
+++ 240205/단어 삭제/word-delete.py
@@ -1,1 +1,10 @@
-print(__import__("sys").stdin.readline().strip().replace(__import__("sys").stdin.readline().strip(), ""))
+import sys
+input = sys.stdin.readline
+
+
+s = input().strip()
+p = input().strip()
+
+for _ in range(len(s)-len(p)):
+    s = s.replace(p, "")
+print(s)
