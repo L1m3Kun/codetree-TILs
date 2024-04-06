e708564a05db96a57fbae281ab989652e081b6bd
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2024-04-06 19:27:57 +0900

    Title: 체질량지수, Time: 194ms, Memory: 14MB, Status: Passed - CodeTree

--- 240406/체질량지수/bmi.js
+++ 240406/체질량지수/bmi.js
@@ -1,3 +1,4 @@
 const [h, w] = require("fs").readFileSync(0).toString().split(" ").map(Number)
 
-const b = (10000*w)/h*h
+const b = parseInt((10000*w)/(h*h))
+console.log(`${b}\n${b>=25?"Obesity":""}`)
