80a5d0b6ada7a042a2e0147308af5b7bfb0f0614
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2024-04-10 22:48:37 +0900

    Title: 비교 연산, Time: 230ms, Memory: 23MB, Status: Passed - CodeTree

---
+++ 240410/비교 연산/comparison-operator.js
@@ -0,0 +1,8 @@
+const [a, b] = require("fs").readFileSync(0).toString().split(" ").map(Number);
+
+console.log(Number(a>=b));
+console.log(Number(a>b));
+console.log(Number(a<=b));
+console.log(Number(a<b));
+console.log(Number(a===b));
+console.log(Number(a!==b));
