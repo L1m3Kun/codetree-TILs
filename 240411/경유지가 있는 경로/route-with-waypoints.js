96434a8e4362239418dda84d2f3933c85c2df6f5
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2024-04-11 02:01:03 +0900

    Title: 경유지가 있는 경로, Time: 213ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240411/경유지가 있는 경로/route-with-waypoints.js
@@ -0,0 +1,23 @@
+const [n, m, k] = require("fs").readFileSync(0).toString().split(" ").map(Number)
+
+const dp = []
+for (let y = 0 ; y < n ; y++){
+    dp.push(Array(m).fill(1))
+}
+for (let y=1;y<n;y++){
+    for (let x=1;x<m;x++){
+        dp[y][x] = dp[y][x-1] + dp[y-1][x-1]
+    }
+}
+
+if (k){
+    let [i,j] = [parseInt(k/m), k%m-1]
+    if (j < 0) {
+        i -= 1
+        j = m-1
+    }
+    
+    console.log(dp[i][j] * dp[n-i-1][m-j-1])
+}else{
+    console.log(dp[n-1][m-1])
+}
