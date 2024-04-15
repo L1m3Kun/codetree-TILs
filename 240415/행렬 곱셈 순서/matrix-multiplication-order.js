dc74d122476cd0ce70352c3b0d2106870531a78e
SeongGu Kang <113879996+L1m3Kun@users.noreply.github.com>
2024-04-15 11:09:24 +0900

    Title: 행렬 곱셈 순서, Time: 218ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240415/행렬 곱셈 순서/matrix-multiplication-order.js
@@ -0,0 +1,38 @@
+const input = require("fs").readFileSync(0).toString().split("\n")
+const N = Number(input[0])
+const nums = []
+for (let i = 1; i <= N ; i++){
+    if (i == 1){
+        nums.push(Number(input[i].split(" ")[0]))
+        nums.push(Number(input[i].split(" ")[1]))
+    }
+    else{
+        nums.push(Number(input[i].split(" ")[1]))
+    }
+}
+
+const dp = []
+
+for (let i = 0; i <= N; i++){
+    dp.push(Array(N+1).fill(0))
+}
+
+
+// for (let i = 0 ; i<= N; i++) dp[i][i] = nums[i]
+
+
+for (let cnt = 0 ; cnt <= N ; cnt++){
+    for (let i = 1; i <= N-cnt ; i++){
+        const j = i + cnt
+        if (cnt > 0){
+            let minv = 250001
+            for (let p = i; p < j ; p++){
+                minv = Math.min(minv, dp[i][p]+dp[p+1][j]+nums[i-1]*nums[p]*nums[j])
+            }
+            dp[i][j] = minv
+        }
+    }
+}
+// console.log(nums)
+// console.log(dp)
+console.log(dp[1][N])
